8c186331fe4dca9f29cae818dea0cb00e8f4b0db
Reece <Uni@192-168-1-10.tpgi.com.au>
2018-11-04 11:56:27 +1100

    added validation type for image and pdf upload

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -73,6 +73,6 @@ class ProductsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:title, :description, :price, :user_id, :image)
+      params.require(:product).permit(:title, :description, :price, :user_id, :image, :file)
     end
 end
