ebe9ef078afe5ce0794b64b3930716c8d3ed3366
Reece <Uni@Reeces-MacBook-Pro-Main.local>
2018-11-05 15:26:43 +1100

    adding order to charges controller

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -17,9 +17,36 @@ class ChargesController < ApplicationController
             :description => @product.title,
             :currency    => 'aud',
         )
- 
-        rescue Stripe::CardError => e
+        
+    rescue Stripe::CardError => e
         flash[:error] = e.message
         redirect_to new_charge_path
+        
+        @order = Order.new(order_params)
+        @order.product_id = @product
+        @order.user = current_user
+        @order.purchase_price = @product.price 
+        @order.save
+        # respond_to do |format|
+    #   if @order.save
+    #     format.html { redirect_to @product, notice: 'Program was successfully purchased.' }
+    #     format.json { render :show, status: :created, location: @product }
+    #   else
+    #     format.html { render :new }
+    #     format.json { render json: @product.errors, status: :unprocessable_entity }
+    #   end
+    end
+
+    private
+    # # Use callbacks to share common setup or constraints between actions.
+    # def set_order
+    #   @product = Product.find(params[:id])
+    # end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def order_params
+      result = params.require(:order).permit(:purchase_price, :user_id, :product_id)
+      result[:purchase_price] = result[:purchase_price].to_f * 100.0
+      result
     end
 end
