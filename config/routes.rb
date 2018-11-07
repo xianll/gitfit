96dae2895de5eecc87b491ba4a9af6befb7db697
Reece <Uni@192-168-1-10.tpgi.com.au>
2018-11-08 00:55:47 +1100

    added new background image for root page and navbar links working for login/out, register and sell program

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   devise_for :users
   resources :profiles
   resources :products
-  post '/charges', to: 'user#account'
+  post '/charges', to: 'user#index'
   resources :charges
-  get '/user/:id', to: 'user#account'
+  get '/user/:id', to: 'user#index'
   get '/user/:id/myproducts', to: 'user#myproducts'
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
