9eaaaf0486450c19fd9ee0c33906e7898995fd2d
Reece <Uni@192-168-1-10.tpgi.com.au>
2018-11-08 14:09:33 +1100

    mailer confirmation fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :validatable, stretches: 12
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable, stretches: 12
   has_many :products
   has_one :profile
   has_many :orders
